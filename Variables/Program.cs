d52e6141c329f9d2bea874f136b178e75e5ec2b3
Miah Harper <harpermiah@yahoo.com>
2022-05-31 15:16:10 -0400

    created variables exercise

---
+++ Variables/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dogName;
+            int dogAge;
+
+            bool theBestDog;
+
+            char shesACharacter;
+
+
+            dogName = "Blue";
+
+            dogAge = 8;
+
+            theBestDog = true;
+
+            shesACharacter = '!';
+
+
+
+
+            Console.WriteLine($"My dogs name is {dogName}, She is {dogAge} years old and she's the best dog, which is {theBestDog} {shesACharacter}");
+        }
+    }
+}
+
